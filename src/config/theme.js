bca1371f00e51f08059e4fe71fecb3f4f7a18bac
Sudipborad <sudipborad1@gmail.com>
2026-02-03 00:25:14 +0530

    Initial structure setup

---
+++ src/config/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    primary: '#007bff',
+    secondary: '#6c757d',
+  },
+};
---
+++ src/services/apiService.js
@@ -0,0 +1,4 @@
+export const apiService = {
+  get: async () => {},
+  post: async () => {},
+};
